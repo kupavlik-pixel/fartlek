1363baa5caf25dbd77a27fea1de5ce54002e4d22
kupavlik-pixel <kupavlik@gmail.com>
2026-08-03 16:02:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
